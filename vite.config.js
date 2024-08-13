af75c57c85c84bfcbcbcead650681dd547518b94
whyrupesh <srupeshkumar800@gmail.com>
2024-08-14 00:12:42 +0530

    fixed typo

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/StemTec/", // Add this line
   plugins: [react()],
 });
